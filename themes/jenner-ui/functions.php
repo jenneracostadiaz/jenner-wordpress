c354a644468a97b39e16dc97c73c2e804ab40308
Jenner Acosta Díaz <my@jenner.pe>
2022-11-24 18:40:00 -0500

    Internacionalizaction Theme

--- themes/jenner-ui/functions.php
+++ themes/jenner-ui/functions.php
@@ -5,14 +5,31 @@
  * @package JennerUI
  * @since JennerUI 1.0
  */
+if ( ! isset( $content_width ) ) {
+	$content_width = 800; /* pixels */
+}
+
 if ( ! function_exists( 'jennerui_setup' ) ) :
 
 	function jennerui_setup() {
 
-		add_theme_support('title-tag');
+		add_theme_support( 'title-tag' );
+		add_theme_support( 'dark-editor-style' );
         add_theme_support( 'automatic-feed-links' );
 		add_theme_support( 'post-thumbnails' );
         add_theme_support( 'align-wide' );
+        add_theme_support( 'automatic-feed-links' );
+        add_theme_support( 'responsive-embeds' );
+        add_theme_support( 'editor-styles' );
+        add_theme_support( 'html5', array( 'style','script' ) );
+        add_theme_support( 'custom-logo', array(
+            'height'               => 50,
+            'width'                => 50,
+            'flex-height'          => true,
+            'flex-width'           => true,
+            'header-text'          => array( 'site-title', 'site-description' ),
+            'unlink-homepage-logo' => true,
+        ) );
         /*
         * Soporte para Medios
         */
@@ -29,18 +46,8 @@ if ( ! function_exists( 'jennerui_setup' ) ) :
         wp_enqueue_script('darkmode', get_template_directory_uri() . '/assets/js/components/darkmode.js', array('jquery'), wp_get_theme()->get('Version'), true);
         wp_enqueue_script('app', get_template_directory_uri() . '/assets/js/app.js', array('darkmode'), wp_get_theme()->get('Version'), true);
 
-        /**
-         * Brand Support Logo 
-        */
-        $defaults = array(
-            'height'               => 50,
-            'width'                => 50,
-            'flex-height'          => true,
-            'flex-width'           => true,
-            'header-text'          => array( 'site-title', 'site-description' ),
-            'unlink-homepage-logo' => true, 
-        );
-        add_theme_support( 'custom-logo', $defaults );
+        /** Intercionalización */
+        load_theme_textdomain( 'jennerui', get_template_directory() . '/languages' );
 
 	}
 endif; // jennerui_setup
