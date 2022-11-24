e81f75af621a513c71d80a0059e22cdaac4035c5
Jenner Acosta Díaz <my@jenner.pe>
2022-11-24 10:07:50 -0500

    Config Gutenberg Plugin

---
+++ plugins/jenner-gutenberg/jenner-gutenberg.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ *  Plugin Name: Jenner's Blocks
+ *  Description: Bloques nativos
+ *  Version: 1.0
+ *  Author: Jenner Acosta Diaz
+ *  Licensie: GPL2
+ */
+
+ if(!defined('ABSPATH')) exit;
