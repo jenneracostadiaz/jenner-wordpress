29b371576bd3d78b397ef52da5658b26c9cc4354
Jenner Acosta Díaz <my@jenner.pe>
2022-11-24 10:45:19 -0500

    Registrar los bloques en el editor

---
+++ plugins/jenner-gutenberg/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '956a995daa03a30aa99f');
--- plugins/jenner-gutenberg/jenner-gutenberg.php
+++ plugins/jenner-gutenberg/jenner-gutenberg.php
@@ -8,3 +8,20 @@
  */
 
  if(!defined('ABSPATH')) exit;
+ /** Registrar Bloques, scripts u CSS */
+
+ function jenner_registrar_bloques(){
+    //Si Gutenberg no existe, salir
+    if(!function_exists('register_block_type')){
+        return;
+    }
+
+    //Registrar los bloques en el editor
+    wp_register_script(
+        'jenner-esitor-script', //nombre unico
+        plugins_url('build/index.js', __FILE__), //archivo con los bloques
+        array('wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor'), //dependencias
+        filemtime( plugin_dir_path(__FILE__) . 'build/index.js') //versión
+    )
+ }
+ add_action( 'init', 'jenner_registrar_bloques' );
