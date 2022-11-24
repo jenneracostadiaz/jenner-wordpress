b8dbe2e6ff430e78fc6c19416c2579ea6559514b
Jenner Acosta Díaz <my@jenner.pe>
2022-11-24 10:50:43 -0500

    Registro de Estilos

--- plugins/jenner-gutenberg/jenner-gutenberg.php
+++ plugins/jenner-gutenberg/jenner-gutenberg.php
@@ -22,6 +22,22 @@
         plugins_url('build/index.js', __FILE__), //archivo con los bloques
         array('wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor'), //dependencias
         filemtime( plugin_dir_path(__FILE__) . 'build/index.js') //versión
-    )
+    );
+
+    //Estilos para el editor (unicamente)
+    wp_register_style(
+        'jenner-editor-styles',
+        plugins_url('build/editors.css', __FILE__),
+        array('wp-edit-blocks'),
+        filemtime( plugin_dir_path(__FILE__) . 'build/editor.css')
+    );
+    
+    //Estilos para los bloques backend y frontend
+    wp_register_style(
+        'jenner-frontend-styles',
+        plugins_url('build/styles.css', __FILE__),
+        array(),
+        filemtime( plugin_dir_path(__FILE__) . 'build/styles.css')
+    );
  }
  add_action( 'init', 'jenner_registrar_bloques' );
