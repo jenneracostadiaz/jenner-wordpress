e2278b7d611b88b0c011d3544b2f762a330c8197
Jenner Acosta Díaz <my@jenner.pe>
2022-11-25 18:36:36 -0500

    Structure HTML Value Proposal

--- themes/jenner-ui/front-page.php
+++ themes/jenner-ui/front-page.php
@@ -2,7 +2,22 @@
 
 <main class="home-page container">
     <?php the_content(); ?>
-    
+    <section class="value-proposal">
+        <ul class="value-proposal__list">
+            <li class="value-proposal__list__item">
+                <h3>Diseña</h3>
+                <p>Explora tu lado creativo, nosotros nos encargamos de convertir todas tus ideas en realidad</p>
+            </li>
+            <li class="value-proposal__list__item">
+                <h3>Desarrolla</h3>
+                <p>Todos tus planes y proyectos con sistemas eficientes y de alta tecnología para un mejor flujo de trabajo</p>
+            </li>
+            <li class="value-proposal__list__item">
+                <h3>Publica</h3>
+                <p>Cumple todas tus metas, estamos acompañadote durante todo el proceso y el el futuro de tu marca.</p>
+            </li>
+        </ul>
+    </section>
 </main>
 
 <?php get_footer(); ?>
